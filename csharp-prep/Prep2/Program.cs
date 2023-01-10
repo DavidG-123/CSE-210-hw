c09438996d46b994b827f5f3177e48a6bf76ecbe
David <source01.file.01@gmail.com>
2023-01-09 18:19:32 -0700

    Prep2 finnished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -3,7 +3,31 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+    {   
+        string letterGrade = "";
+        Console.Write("What is your grade percent value? (write as rounded integer!) "); float grade = Int32.Parse(Console.ReadLine());
+        if (grade >= 90) {
+            letterGrade = (grade + "%");
+        }
+        else if (grade < 90 && grade >= 80) {
+            letterGrade = (grade + "%");
+        }
+        else if (grade >= 70) {
+            letterGrade = (grade + "%");
+        }
+        else if (grade >= 60) {
+            letterGrade = (grade + "%");
+        }
+        else if (grade < 60) {
+            letterGrade = (grade + "%");
+        }
+
+
+        if (grade >= 70 ) {
+            Console.WriteLine($"Your grade is {letterGrade}, you passed!");
+        }
+        else if (grade < 70) {
+            Console.WriteLine($"Your grade is {letterGrade}. You didn't pass, but there's always next time :) !");
+        }
     }
 }
