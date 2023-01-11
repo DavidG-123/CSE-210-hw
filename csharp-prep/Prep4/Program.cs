e3d5a3cea2e4f45d142f96666434e50505f4f476
David <source01.file.01@gmail.com>
2023-01-11 12:38:20 -0700

    Prep4 finnished

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        Console.Write("Enter number: ");
+        string userNumber = Console.ReadLine();
+        List<int> numbers = new List<int>();
+        numbers.Add(Int16.Parse(userNumber));
+        AddIt(numbers);
+    }
+    static void Product(List<int> args)
+    {
+        int sum = 0;
+        foreach (int additive in args)
+        {
+            sum += additive;
+        }
+        int max = args[0];
+        foreach (int number in args)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {sum/args.Count}");
+        Console.WriteLine($"The largest number is: {max}");
+
+    }
+    static void AddIt(List<int> args){
+        Console.Write("Enter number: ");
+        int userNumber = Int16.Parse(Console.ReadLine());
+        if (args[(args.Count)-1] == 0){
+            Product(args);
+        }
+        else if (userNumber != 0)
+        {
+            args.Add(userNumber);
+            AddIt(args);
+        }
+        else {
+            Product(args);
+        }
+        
     }
 }
