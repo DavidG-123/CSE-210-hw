84dc2bd8a72a7567b2d810dbae1974f1f57fcb3f
David George <source01.file.01@gmail.com>
2023-01-18 22:09:55 -0700

    Learning02 completed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Console.WriteLine($"Welcome to Resume Engine(TM)\n\n");
+        
+        Job job1 = new Job();
+        job1._jobTitle = "Brick Layer";
+        job1._company = "OSHA";
+        job1._startDate = "2003";
+        job1._endDate = "2018";
+        
+        Job job2 = new Job();
+        job2._jobTitle = "Bomb Squad K-9 Handler";
+        job2._company = "US Army";
+        job2._startDate = "2018";
+        job2._endDate = "2023";
+
+
+        Resume myResume = new Resume();
+        myResume._usersName = "Ally Caustik";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        
+        myResume.Display();
     }
+    
+    // static void (string[] args)
+    // {
+    //     Console.Write("What is your job title? ");
+    //     string jobValue = Console.ReadLine();
+    //     Console.Write("What company do you work for? ");
+    //     string companyName = Console.ReadLine();
+    //     Console.Write("What year did you start? ");
+    // }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Resume
+{
+    public string _usersName;
+    public List<Job> _jobs = new List<Job>();
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_usersName}\nJobs: ");
+        _jobs[0].DisplayJobInfo();
+        _jobs[1].DisplayJobInfo();        
+    }
+}
