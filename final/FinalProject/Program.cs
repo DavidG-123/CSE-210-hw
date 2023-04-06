d823d2a469a89c4ea544fec839a7253a27177d11
David <source01.file.01@gmail.com>
2023-04-06 09:01:29 -0600

    final

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -18,26 +18,23 @@ class Program
         IPAddress[] localIPs = Dns.GetHostAddresses(hostName);
         
         
-        TcpListener server = new TcpListener(IPAddress.Any, 9999);  
-        // we set our IP address as server's address, and we also set the port: 9999
+        TcpListener server = new TcpListener(IPAddress.Any, 9999);
         
-        server.Start();  // this will start the server
+        server.Start();
 
         string compileMessage1 = "";
 
-        while (true)   //we wait for a connection
+        while (true)
         
         {
-            TcpClient client = server.AcceptTcpClient();  //if a connection exists, the server will accept it
-            NetworkStream ns = client.GetStream(); //networkstream is used to send/receive messages
+            TcpClient client = server.AcceptTcpClient();
+            NetworkStream ns = client.GetStream();
             IPEndPoint remoteEndPoint = (IPEndPoint)client.Client.RemoteEndPoint;
-            //byte[] hello = new byte[100];   //any message must be serialized (converted to byte array)
-            //hello = Encoding.Default.GetBytes("hello world");  //conversion string => byte array
-            //ns.Write(hello, 0, hello.Length);     //sending the message
-            while (client.Connected)  //while the client is connected, we look for incoming messages
+            
+            while (client.Connected) 
             {
-                byte[] msg = new byte[1024];     //the messages arrive as byte array
-                ns.Read(msg, 0, msg.Length);   //the same networkstream reads the message sent by the client
+                byte[] msg = new byte[1024];
+                ns.Read(msg, 0, msg.Length);
                 var encoding = Encoding.UTF8;
                 compileMessage1 += encoding.GetString(msg);
                 foreach (var g in compileMessage1) {
